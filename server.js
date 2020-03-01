e2b61d8b051b1ef3bf8cb601480d9e9b41121897
Filip <filip.turkiewicz@gmail.com>
2020-03-01 17:15:36 +0100

    run ng on heroku

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/ui'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ui'));
 
-// Send all requests to index.html
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ui/index.html'));
+
+    res.sendFile(path.join(__dirname, '/dist/ui/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
